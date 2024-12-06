b328dfe49b38a8f5b654aace864742765a10c8f2
Christian Brüggemann <mail@cbruegg.com>
2024-12-06 22:24:06 +0100

    AoC 06/A: Fix small bug

--- src/main/kotlin/aoc06/GuardGallivantPartA.kt
+++ src/main/kotlin/aoc06/GuardGallivantPartA.kt
@@ -11,7 +11,7 @@ fun main() {
     var (obstacles, curX, curY, direction) = parseInitialGameState(lines)
 
     val visitedPositions = mutableSetOf<Pair<Int, Int>>()
-    while (curX < width && curY < height) {
+    while (curX in 0..<width && curY in 0..<height) {
         visitedPositions += Pair(curX, curY)
 
         val (vecX, vecY) = direction.vector
@@ -27,63 +27,4 @@ fun main() {
     }
 
     println(visitedPositions.size)
-}
-
-data class GuardGameState(
-    val obstacles: Set<Pair<Int, Int>>,
-    var curX: Int,
-    var curY: Int,
-    var direction: Direction
-)
-
-fun parseInitialGameState(lines: List<String>): GuardGameState {
-    val obstacles = mutableSetOf<Pair<Int, Int>>()
-    var curX = -1
-    var curY = -1
-    lateinit var direction: Direction
-    for ((y, line) in lines.withIndex()) {
-        for ((x, char) in line.withIndex()) {
-            when (char) {
-                '#' -> obstacles += Pair(x, y)
-                '^' -> {
-                    curX = x
-                    curY = y
-                    direction = Direction.Up
-                }
-
-                '>' -> {
-                    curX = x
-                    curY = y
-                    direction = Direction.Right
-                }
-
-                'v' -> {
-                    curX = x
-                    curY = y
-                    direction = Direction.Down
-                }
-
-                '<' -> {
-                    curX = x
-                    curY = y
-                    direction = Direction.Left
-                }
-            }
-        }
-    }
-    return GuardGameState(obstacles, curX, curY, direction)
-}
-
-enum class Direction(val vector: Pair<Int, Int>) {
-    Left(-1 to 0),
-    Up(0 to -1), // -1 as our top is index 0
-    Right(1 to 0),
-    Down(0 to 1)
-}
-
-fun Direction.turnRight() = when (this) {
-    Direction.Left -> Direction.Up
-    Direction.Up -> Direction.Right
-    Direction.Right -> Direction.Down
-    Direction.Down -> Direction.Left
 }
