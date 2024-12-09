05164eedd7eb12cef60b11ca74936b6f1b67291b
Christian Brüggemann <mail@cbruegg.com>
2024-12-09 21:50:23 +0100

    AoC 09/A: Solution for challenge

--- src/main/kotlin/aoc09/DiskFragmenter.kt
+++ src/main/kotlin/aoc09/DiskFragmenter.kt
@@ -1,57 +1,65 @@
 package aoc09
 
+import java.io.File
+
+// -1 represents free space
+typealias DiskMap = IntArray
+
 fun main() {
-    val input = "2333133121414131402"
+    val input = File("inputs/aoc09/diskmap.txt").readText().trim()
 
     val diskMap = expandDiskMap(input)
-    println("diskMap=$diskMap")
+    println("diskMap=${diskMap.contentToString()}")
 
     val optimizedDiskMap = optimizeSpaceUsage(diskMap)
-    println("optimizedDiskMap=$optimizedDiskMap")
+    println("optimizedDiskMap=${optimizedDiskMap.contentToString()}")
 
     val checksum = checksum(optimizedDiskMap)
     println("checksum=$checksum")
 }
 
-fun checksum(diskMap: String): Long {
+fun checksum(diskMap: DiskMap): Long {
     var sum = 0L
-    for ((idx, fileIdStr) in diskMap.withIndex()) {
-        if (fileIdStr in '0'..'9') {
-            val fileId = fileIdStr.digitToInt()
+    for ((idx, fileId) in diskMap.withIndex()) {
+        if (fileId != -1) {
             sum += fileId * idx
         }
     }
     return sum
 }
 
-fun optimizeSpaceUsage(originalDiskMap: String): String {
-    val diskMap = originalDiskMap.toCharArray()
+fun optimizeSpaceUsage(originalDiskMap: DiskMap): DiskMap {
+    val diskMap = originalDiskMap.copyOf()
     for (i in diskMap.indices.reversed()) {
         val block = diskMap[i]
-        if (block != '.') {
-            val freeBlockIdx = diskMap.indexOf('.') // we could optimize this by going from left to right, but whatever
+        if (block != -1) {
+            val freeBlockIdx = diskMap.indexOf(-1) // we could optimize this by going from left to right, but whatever
             if (freeBlockIdx == -1 || freeBlockIdx > i) {
                 // No more free space
                 break
             }
             diskMap[freeBlockIdx] = block
-            diskMap[i] = '.'
+            diskMap[i] = -1
         }
     }
-    return diskMap.concatToString()
+    return diskMap
 }
 
-fun expandDiskMap(input: String): String =
-    buildString {
+fun expandDiskMap(input: String): DiskMap =
+    buildList {
         for ((idx, blockLengthStr) in input.withIndex()) {
             val blockLength = blockLengthStr.digitToInt()
             if (idx % 2 == 0) {
                 // File
                 val fileId = idx / 2
-                repeat(fileId.digitToChar().code, blockLength)
+                repeat(blockLength) {
+                    add(fileId)
+                }
             } else {
                 // Free space
-                repeat('.'.code, blockLength)
+                repeat(blockLength) {
+                    add(-1)
+                }
             }
         }
-    }
+    }.toIntArray()
