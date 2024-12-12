31c58f01a918049bd54d2c42690798ee4815a48e
Christian Brüggemann <mail@cbruegg.com>
2024-12-12 23:23:03 +0100

    AoC 11/B: Attempt to solve with state file (failed)

--- src/main/kotlin/aoc11/PlutonianPebbles.kt
+++ src/main/kotlin/aoc11/PlutonianPebbles.kt
@@ -1,41 +1,64 @@
 package aoc11
 
 import java.io.File
-import kotlin.math.log10
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardCopyOption
+import kotlin.io.path.bufferedWriter
+import kotlin.io.path.forEachLine
+import kotlin.io.path.writeText
 import kotlin.math.pow
 
 fun main() {
-    val input = File("inputs/aoc11/input.txt").readText().trim()
+    val input = File("inputs/aoc11/input.txt")
+    // replace spaces with newlines to be able to read line by line later
+    val inputLines = input.readText().split(' ').joinToString(separator = "\n")
 
-    var state: List<Long> = input.split(' ').map { it.toLong() }
-    println(state)
-    repeat(25) {
-        val nextState = ArrayList<Long>(/* initialCapacity = */ (state.size * 1.5).toInt())
-        for (n in state) {
-            val numOfDigitsInBase10 = n.numOfDigitsInBase10
-            when {
-                n == 0L -> {
-                    nextState += 1
-                }
+    val stateFile = Files.createTempFile("pebbles", ".txt")
+    stateFile.writeText(inputLines)
+    val nextStateFile = Files.createTempFile("pebbles-next", ".txt")
 
-                numOfDigitsInBase10 % 2 == 0 -> {
-                    // Do this with number types instead of Strings just for fun
-                    val moveRightDivisor = 10.0.pow(numOfDigitsInBase10 / 2).toLong()
-                    val leftHalf = n / moveRightDivisor
-                    val rightHalf = n - leftHalf * moveRightDivisor
-                    nextState += leftHalf
-                    nextState += rightHalf
-                }
+    println(stateFile)
+    println(nextStateFile)
 
-                else -> {
-                    nextState += n * 2024
+    repeat(75) { i ->
+        println("Iteration $i")
+        nextStateFile.bufferedWriter().use { nextStateFileWriter ->
+            for (n in readNumbers(stateFile)) {
+                check(n >= 0)
+                val numOfDigitsInBase10 = n.numOfDigitsInBase10
+                when {
+                    n == 0L -> {
+                        nextStateFileWriter.appendLine('1')
+                    }
+
+                    numOfDigitsInBase10 % 2 == 0 -> {
+                        // Do this with number types instead of Strings just for fun
+                        val moveRightDivisor = 10.0.pow(numOfDigitsInBase10 / 2).toLong()
+                        val leftHalf = n / moveRightDivisor
+                        val rightHalf = n - leftHalf * moveRightDivisor
+                        nextStateFileWriter.appendLine(leftHalf.toString())
+                        nextStateFileWriter.appendLine(rightHalf.toString())
+                    }
+
+                    else -> {
+                        nextStateFileWriter.appendLine((n * 2024).toString())
+                    }
                 }
             }
         }
-        state = nextState
-        // println(state)
+        Files.move(nextStateFile, stateFile, StandardCopyOption.REPLACE_EXISTING)
     }
-    println(state.size)
+    println("Counting numbers...")
+    println(readNumbers(stateFile).count())
 }
 
-private val Long.numOfDigitsInBase10 get() = log10(this.toDouble()).toInt() + 1
+private fun readNumbers(path: Path): Sequence<Long> =
+    sequence {
+        path.forEachLine { line ->
+            yield(line.toLong())
+        }
+    }
+
+//private val Long.numOfDigitsInBase10 get() = log10(this.toDouble()).toInt() + 1
+private val Long.numOfDigitsInBase10 get() = toString().length
