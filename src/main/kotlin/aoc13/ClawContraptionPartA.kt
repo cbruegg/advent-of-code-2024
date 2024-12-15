0f613ed7e75e4864fd7ec14c4156291ce3222d5b
Christian Brüggemann <mail@cbruegg.com>
2024-12-15 13:13:37 +0100

    AoC 13/A: Solution for challenge

--- src/main/kotlin/aoc13/ClawContraptionPartA.kt
+++ src/main/kotlin/aoc13/ClawContraptionPartA.kt
@@ -1,23 +1,9 @@
 package aoc13
 
+import java.io.File
+
 fun main() {
-    val input = """
-        Button A: X+94, Y+34
-        Button B: X+22, Y+67
-        Prize: X=8400, Y=5400
-
-        Button A: X+26, Y+66
-        Button B: X+67, Y+21
-        Prize: X=12748, Y=12176
-
-        Button A: X+17, Y+86
-        Button B: X+84, Y+37
-        Prize: X=7870, Y=6450
-
-        Button A: X+69, Y+23
-        Button B: X+27, Y+71
-        Prize: X=18641, Y=10279
-    """.trimIndent()
+    val input = File("inputs/aoc13/input.txt").readText()
 
     val games = parseClawGames(input)
     val solutionsByGame = games.map { it.solve() }
