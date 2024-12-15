d8615c09ff9284dc228947ad59bc49f99e35a226
Christian Brüggemann <mail@cbruegg.com>
2024-12-15 14:09:08 +0100

    AoC 14/A: Solution for challenge

--- src/main/kotlin/aoc14/RestroomRedoubtPartA.kt
+++ src/main/kotlin/aoc14/RestroomRedoubtPartA.kt
@@ -1,24 +1,12 @@
 package aoc14
 
 import aoc12.Coordinate
+import java.io.File
 
 fun main() {
-    val input = """
-        p=0,4 v=3,-3
-        p=6,3 v=-1,-3
-        p=10,3 v=-1,2
-        p=2,0 v=2,-1
-        p=0,0 v=1,3
-        p=3,0 v=-2,-2
-        p=7,6 v=-1,-3
-        p=3,0 v=-1,-2
-        p=9,3 v=2,3
-        p=7,3 v=-1,2
-        p=2,4 v=2,-3
-        p=9,5 v=-3,-3
-    """.trimIndent().lines()
-    val width = 11 // 101 in real input
-    val height = 7 // 103 in real input
+    val input = File("inputs/aoc14/input.txt").readLines()
+    val width = 101 // 101 in real input, 11 in example
+    val height = 103 // 103 in real input, 7 in example
 
     val regex = Regex("""p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)""")
     var robotPositions = input.map { robotSpec ->
