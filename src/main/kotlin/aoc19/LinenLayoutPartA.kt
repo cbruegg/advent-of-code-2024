d40d116395f2423187e04280150b98a2bd8aa801
Christian Brüggemann <mail@cbruegg.com>
2024-12-20 16:54:53 +0100

    AoC 19/A: Solution for example

---
+++ src/main/kotlin/aoc19/LinenLayoutPartA.kt
@@ -0,0 +1,33 @@
+package aoc19
+
+fun main() {
+    val input = """
+        r, wr, b, g, bwu, rb, gb, br
+
+        brwrr
+        bggr
+        gbbr
+        rrbgbr
+        ubwu
+        bwurrg
+        brgr
+        bbrgwb
+    """.trimIndent().lines()
+
+    val towels = input[0].split(", ").sortedByDescending { it.length }
+    val designs = input.drop(2)
+
+    val possibleDesigns = designs.filter { isDesignPossible(it, towels) }
+    println(possibleDesigns.size)
+}
+
+fun isDesignPossible(design: String, towels: List<String>): Boolean {
+    if (design.isEmpty()) return true
+
+    for (towel in towels) {
+        if (design.startsWith(towel) && isDesignPossible(design.substring(towel.length), towels)) {
+            return true
+        }
+    }
+    return false
+}
