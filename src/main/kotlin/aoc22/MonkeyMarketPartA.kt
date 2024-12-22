d2d3e0322c30134d8ac75deafa45f400ea880fdf
Christian Brüggemann <mail@cbruegg.com>
2024-12-22 14:37:43 +0100

    AoC 22/A: Solution for challenge

--- src/main/kotlin/aoc22/MonkeyMarketPartA.kt
+++ src/main/kotlin/aoc22/MonkeyMarketPartA.kt
@@ -1,18 +1,18 @@
 package aoc22
 
+import java.io.File
+
 fun main() {
-    val input = """
-        1
-        10
-        100
-        2024
-    """.trimIndent().lines()
+//    val input = """
+//        1
+//        10
+//        100
+//        2024
+//    """.trimIndent().lines()
+    val input = File("inputs/aoc22/input.txt").readLines()
 
     val sumOf2000thSecret = input.map { it.toLong() }.sumOf { seed -> prng(seed).drop(1999).first() }
     println(sumOf2000thSecret)
-
-    val next = prng(seed = 123).take(10).toList()
-    println(next)
 }
 
 fun prng(seed: Long): Sequence<Long> = sequence {
