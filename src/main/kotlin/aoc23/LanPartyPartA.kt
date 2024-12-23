48b0ee06a0b04223b31e9597fb579d5a96b57d72
Christian Brüggemann <mail@cbruegg.com>
2024-12-23 16:40:52 +0100

    AoC 23/A: Solution for challenge

--- src/main/kotlin/aoc23/LanPartyPartA.kt
+++ src/main/kotlin/aoc23/LanPartyPartA.kt
@@ -1,40 +1,9 @@
 package aoc23
 
+import java.io.File
+
 fun main() {
-    val input = """
-        kh-tc
-        qp-kh
-        de-cg
-        ka-co
-        yn-aq
-        qp-ub
-        cg-tb
-        vc-aq
-        tb-ka
-        wh-tc
-        yn-cg
-        kh-ub
-        ta-co
-        de-co
-        tc-td
-        tb-wq
-        wh-td
-        ta-ka
-        td-qp
-        aq-cg
-        wq-ub
-        ub-vc
-        de-ta
-        wq-aq
-        wq-vc
-        wh-yn
-        ka-de
-        kh-ta
-        co-tc
-        wh-qp
-        tb-vc
-        td-yn
-    """.trimIndent().lines()
+    val input = File("inputs/aoc23/input.txt").readLines()
 
     val pcsToNeighbors = input
         .asSequence()
@@ -58,4 +27,5 @@ fun main() {
     for (clique in cliquesOf3.sortedBy { it.joinToString(separator = "") }) {
         println(clique)
     }
+    println(cliquesOf3.size)
 }
