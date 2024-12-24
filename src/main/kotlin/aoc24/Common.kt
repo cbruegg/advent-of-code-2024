6f8eba69851e0031470f8b2afef586de5e9766a6
Christian Brüggemann <mail@cbruegg.com>
2024-12-24 10:58:31 +0100

    AoC 24/B: Refactoring

---
+++ src/main/kotlin/aoc24/Common.kt
@@ -0,0 +1,54 @@
+package aoc24
+
+enum class GateType { AND, XOR, OR }
+
+data class Gate(val inputA: String, val inputB: String, val output: String, val type: GateType)
+
+fun Gate.evaluate(wireToValue: Map<String, Int>): Int =
+    type.evaluate(wireToValue.getValue(inputA), wireToValue.getValue(inputB))
+
+fun GateType.evaluate(inputA: Int, inputB: Int): Int =
+    when (this) {
+        GateType.AND -> inputA and inputB
+        GateType.XOR -> inputA xor inputB
+        GateType.OR -> inputA or inputB
+    }
+
+fun runCircuit(wireToInitialValue: Map<String, Int>, gates: Collection<Gate>): Map<String, Int> {
+    val wireToAffectedGates = mutableMapOf<String, MutableSet<Gate>>()
+    for (gate in gates) {
+        wireToAffectedGates.getOrPut(gate.inputA, { mutableSetOf() }) += gate
+        wireToAffectedGates.getOrPut(gate.inputB, { mutableSetOf() }) += gate
+    }
+
+    val wireToValue = mutableMapOf<String, Int>()
+    val valueProcessingQueue = wireToInitialValue.entries
+        .map { (wire, value) -> Pair(wire, value) }
+        .toCollection(ArrayDeque())
+
+    while (valueProcessingQueue.isNotEmpty()) {
+        val (wire, value) = valueProcessingQueue.removeFirst()
+        wireToValue[wire] = value
+        val affectedGates = wireToAffectedGates[wire] ?: emptySet()
+        for (affectedGate in affectedGates) {
+            val a = wireToValue[affectedGate.inputA] ?: continue // wait for missing value
+            val b = wireToValue[affectedGate.inputB] ?: continue // wait for missing value
+            valueProcessingQueue += affectedGate.output to affectedGate.type.evaluate(a, b)
+        }
+    }
+
+    return wireToValue
+}
+
+fun collectBits(wireToValue: Map<String, Int>, prefix: String = "z"): Long {
+    val zEntries = wireToValue.filterKeys { it.startsWith(prefix) }
+        .entries
+        .sortedBy { it.key }
+
+    var decimal = 0L
+    for ((i, entry) in zEntries.withIndex()) {
+        val (_, value) = entry
+        decimal = decimal or (value.toLong() shl i)
+    }
+    return decimal
+}
--- src/main/kotlin/aoc24/CrossedWiresPartB.kt
+++ src/main/kotlin/aoc24/CrossedWiresPartB.kt
@@ -1,9 +1,27 @@
 package aoc24
 
-import java.io.File
-
 fun main() {
-    val input = File("inputs/aoc24/input.txt").readText()
+    val input = """
+        x00: 0
+        x01: 1
+        x02: 0
+        x03: 1
+        x04: 0
+        x05: 1
+        y00: 0
+        y01: 0
+        y02: 1
+        y03: 1
+        y04: 0
+        y05: 1
+
+        x00 AND y00 -> z05
+        x01 AND y01 -> z02
+        x02 AND y02 -> z01
+        x03 AND y03 -> z03
+        x04 AND y04 -> z04
+        x05 AND y05 -> z00
+    """.trimIndent()
 
     val (initialValuesStr, gatesStr) = input.split("\n\n")
     val wireToInitialValue = initialValuesStr.lines()
@@ -13,6 +31,10 @@ fun main() {
         .map { it.split(' ') }
         .map { (inputA, type, inputB, _, output) -> Gate(inputA, inputB, output, GateType.valueOf(type)) }
 
+    val x = collectBits(wireToInitialValue, "x")
+    val y = collectBits(wireToInitialValue, "y")
+    val expectedZ = x and y
+
     val wireToValue = runCircuit(wireToInitialValue, gates)
 
     val zEntries = wireToValue.filterKeys { it.startsWith("z") }
@@ -20,7 +42,7 @@ fun main() {
         .sortedBy { it.key }
     zEntries.forEach { (wire, value) -> println("$wire: $value") }
 
-    val decimal = collectBits(wireToValue)
+    val z = collectBits(wireToValue)
 
-    println("decimal=$decimal")
+    println("x=$x, y=$y, z=$z, expectedZ=$expectedZ")
 }
