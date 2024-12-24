2677b702c7db60fe682dbf534c9adb142cd6a8b2
Christian Brüggemann <mail@cbruegg.com>
2024-12-24 10:37:51 +0100

    AoC 24/A: Solution for bigger example

--- src/main/kotlin/aoc24/CrossedWiresPartA.kt
+++ src/main/kotlin/aoc24/CrossedWiresPartA.kt
@@ -3,15 +3,52 @@ package aoc24
 fun main() {
     val input = """
         x00: 1
-        x01: 1
+        x01: 0
         x02: 1
-        y00: 0
+        x03: 1
+        x04: 0
+        y00: 1
         y01: 1
-        y02: 0
+        y02: 1
+        y03: 1
+        y04: 1
 
-        x00 AND y00 -> z00
-        x01 XOR y01 -> z01
-        x02 OR y02 -> z02
+        ntg XOR fgs -> mjb
+        y02 OR x01 -> tnw
+        kwq OR kpj -> z05
+        x00 OR x03 -> fst
+        tgd XOR rvg -> z01
+        vdt OR tnw -> bfw
+        bfw AND frj -> z10
+        ffh OR nrd -> bqk
+        y00 AND y03 -> djm
+        y03 OR y00 -> psh
+        bqk OR frj -> z08
+        tnw OR fst -> frj
+        gnj AND tgd -> z11
+        bfw XOR mjb -> z00
+        x03 OR x00 -> vdt
+        gnj AND wpb -> z02
+        x04 AND y00 -> kjc
+        djm OR pbm -> qhw
+        nrd AND vdt -> hwm
+        kjc AND fst -> rvg
+        y04 OR y02 -> fgs
+        y01 AND x02 -> pbm
+        ntg OR kjc -> kwq
+        psh XOR fgs -> tgd
+        qhw XOR tgd -> z09
+        pbm OR djm -> kpj
+        x03 XOR y03 -> ffh
+        x00 XOR y04 -> ntg
+        bfw OR bqk -> z06
+        nrd XOR fgs -> wpb
+        frj XOR qhw -> z04
+        bqk OR frj -> z07
+        y03 OR x01 -> nrd
+        hwm AND bqk -> z03
+        tgd XOR rvg -> z12
+        tnw OR pbm -> gnj
     """.trimIndent()
 
     val (initialValuesStr, gatesStr) = input.split("\n\n")
@@ -22,13 +59,26 @@ fun main() {
         .map { it.split(' ') }
         .map { (inputA, type, inputB, _, output) -> Gate(inputA, inputB, output, GateType.valueOf(type)) }
 
-    var wireToValue = wireToInitialValue
-    repeat(1) {
-        val nextWireToValue = wireToValue.toMutableMap()
-        for (gate in gates) {
-            nextWireToValue[gate.output] = gate.evaluate(wireToValue)
+    val wireToAffectedGates = mutableMapOf<String, MutableSet<Gate>>()
+    for (gate in gates) {
+        wireToAffectedGates.getOrPut(gate.inputA, { mutableSetOf() }) += gate
+        wireToAffectedGates.getOrPut(gate.inputB, { mutableSetOf() }) += gate
+    }
+
+    val wireToValue = mutableMapOf<String, Int>()
+    val valueProcessingQueue = wireToInitialValue.entries
+        .map { (wire, value) -> Pair(wire, value) }
+        .toCollection(ArrayDeque())
+
+    while (valueProcessingQueue.isNotEmpty()) {
+        val (wire, value) = valueProcessingQueue.removeFirst()
+        wireToValue[wire] = value
+        val affectedGates = wireToAffectedGates[wire] ?: emptySet()
+        for (affectedGate in affectedGates) {
+            val a = wireToValue[affectedGate.inputA] ?: continue // wait for missing value
+            val b = wireToValue[affectedGate.inputB] ?: continue // wait for missing value
+            valueProcessingQueue += affectedGate.output to affectedGate.type.evaluate(a, b)
         }
-        wireToValue = nextWireToValue
     }
 
     val zEntries = wireToValue.filterKeys { it.startsWith("z") }
@@ -50,8 +100,11 @@ enum class GateType { AND, XOR, OR }
 data class Gate(val inputA: String, val inputB: String, val output: String, val type: GateType)
 
 fun Gate.evaluate(wireToValue: Map<String, Int>): Int =
-    when (type) {
-        GateType.AND -> wireToValue.getValue(inputA) and wireToValue.getValue(inputB)
-        GateType.XOR -> wireToValue.getValue(inputA) xor wireToValue.getValue(inputB)
-        GateType.OR -> wireToValue.getValue(inputA) or wireToValue.getValue(inputB)
+    type.evaluate(wireToValue.getValue(inputA), wireToValue.getValue(inputB))
+
+fun GateType.evaluate(inputA: Int, inputB: Int): Int =
+    when (this) {
+        GateType.AND -> inputA and inputB
+        GateType.XOR -> inputA xor inputB
+        GateType.OR -> inputA or inputB
     }
